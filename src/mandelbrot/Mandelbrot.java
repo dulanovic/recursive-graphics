22f6052f19715a61a3a7f56bbeb77cb47599f6f2
dulanovic <vidan134@live.com>
2018-10-31 14:39:18 +0100

    Mandelbrot.java added

---
+++ src/mandelbrot/Mandelbrot.java
@@ -0,0 +1,14 @@
+package mandelbrot;
+
+public class Mandelbrot {
+
+    private Mandelbrot() {}
+
+    public static void draw() {
+        // to be implemented
+    }
+
+    public static void main(String[] args) {
+        // testing purposes only
+    }
+}
